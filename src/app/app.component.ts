315fb5ba888fdf9411047a2fc9fc1344ebe79058
Your Name <you@example.com>
2020-04-15 18:16:15 -0700

    finish exam 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,19 @@ import { FlexModalService } from './shared-components/flex-modal/flex-modal.serv
 })
 export class AppComponent implements OnInit {
   title = 'final';
-  aboutText: string;
+  aboutText = 'Created by Nareg Apkarian (C) 2020'
   constructor(private flexModal: FlexModalService) {
 
   }
 
   ngOnInit() {
+  
   }
 
+
+  showHelpText() {
+    this.flexModal.openDialog('about-modal');
+    // alert(this.aboutText)
+  }
 
 }
--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -12,6 +12,9 @@ import { Http } from '@angular/http';
 export class OrdersComponent implements OnInit {
 
   orders: Array<any> = [];
+  name: string;
+  errorMessage = '';
+  confirmMessage = '';
 
   constructor(
     private router: Router,
@@ -21,7 +24,105 @@ export class OrdersComponent implements OnInit {
   }
 
   async ngOnInit() {
+    this.loadDefaultOrders();
+  }
+
+
+  calculate() {
+    const total = this.orders.reduce((inc, item, i, arr) => {
+      inc += item.price * item.quantity;
+      return inc;
+    }, 0);
+    const taxAmount = total * .1;
+
+    const subTotal = total - taxAmount;
+    return {
+      total: total,
+      taxAmount: taxAmount,
+      subTotal: subTotal
+    };
+  }
+
+  submit() {
+    const commaIndex = this.name.indexOf(', ');
+    let error = false;
+
+    if (this.name === '') {
+      this.errorMessage = 'Name cannot be empty';
+      error = true;
+    } else if (commaIndex === -1) {
+      this.errorMessage = 'Name must have a comma';
+      error = true;
+    }
+    if (!error) {
+      const firstName = this.name.slice(commaIndex + 1, this.name.length);
+      const lastName = this.name.slice(0, commaIndex);
+      const fullName = firstName + ' ' + lastName;
+
+      const calculation = this.calculate();
+      this.confirmMessage = `Thanks for your order! ${fullName} Your subtotal is : ${calculation.subTotal}. Your tax ammount is ${calculation.taxAmount}. The total for the order is ${calculation.total}. `
+      this.flexModal.openDialog('confirm-modal');
+    } else {
+      this.flexModal.openDialog('error-modal');
+    }
 
   }
 
+  loadDefaultOrders() {
+    this.orders = [{
+      "pid": "1",
+      "image": "assets/sm_android.jpeg",
+      "description": "Android",
+      "price": 150.00,
+      "quantity": 2
+    }, {
+      "pid": "2",
+      "image": "assets/sm_iphone.jpeg",
+      "description": "IPhone",
+      "price": 200.00,
+      "quantity": 1
+    }, {
+      "pid": "3",
+      "image": "assets/sm_windows.jpeg",
+      "description": "Windows Phone",
+      "price": 110.00,
+      "quantity": 2
+    }]
+
+  }
+  delete(index: number) {
+    this.orders.splice(index, 1);
+  }
+
+  addItem(item: string) {
+    switch (item) {
+      case 'Android':
+        this.orders.unshift({
+          "pid": "1",
+          "image": "assets/sm_android.jpeg",
+          "description": "Android",
+          "price": 150.00,
+          "quantity": 0
+        });
+        break;
+      case 'IPhone':
+        this.orders.unshift({
+          "pid": "2",
+          "image": "assets/sm_iphone.jpeg",
+          "description": "IPhone",
+          "price": 200.00,
+          "quantity": 0
+        });
+        break;
+      case 'Windows Phone':
+        this.orders.unshift({
+          "pid": "3",
+          "image": "assets/sm_windows.jpeg",
+          "description": "Windows Phone",
+          "price": 110.00,
+          "quantity": 0
+        });
+        break;
+    }
+  }
 }
